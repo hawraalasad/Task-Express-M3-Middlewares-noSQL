b1c190796762b82374d2e51f3dea2a6369f24ae3
Hawraa <hawraalasad@yahoo.com>
2024-09-26 16:19:40 +0300

    done

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const postsRoutes = require('./api/posts/posts.routes');
-const connectDb = require('./database');
+const postsRoutes = require("./api/posts/posts.routes");
+const morgan = require("morgan");
+const cors = require("cors");
+const connectDb = require("./database");
 
 connectDb();
 app.use(express.json());
-app.use('/posts', postsRoutes);
-
+app.use("/posts", postsRoutes);
+app.use(morgan("dev"));
+app.use(cors());
 app.listen(8000, () => {
-  console.log('The application is running on localhost:8000');
+  console.log("The application is running on localhost:8000");
 });
